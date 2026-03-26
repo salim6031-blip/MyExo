b6796353c30b6b64684497f6e1bc6521d86806aa
salim6031_git <salim6031@gmail.com>
2026-03-26 13:19:52 +0300

    Обновлено Media3 до версии 1.9.1
    Signed-off-by: salim6031_git <salim6031@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,9 @@ android {
     defaultConfig {
         applicationId = "com.example.myexo1"
         minSdk = 25
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
-        versionName = "2.3"
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,16 +40,15 @@ android {
 
 dependencies {
     implementation ("com.github.bumptech.glide:glide:5.0.5")
-    implementation("androidx.media3:media3-ui:1.9.3")
     annotationProcessor ("com.github.bumptech.glide:compiler:5.0.5")
-    implementation("androidx.media3:media3-exoplayer:1.6.1")
-    implementation("androidx.media3:media3-exoplayer-hls:1.6.1")
-    implementation("androidx.media3:media3-exoplayer-dash:1.6.1")
-    implementation("androidx.media3:media3-ui:1.6.1")
-    implementation("org.jellyfin.media3:media3-ffmpeg-decoder:1.6.1+2")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.media3:media3-exoplayer:1.9.3")
+    implementation("androidx.media3:media3-exoplayer-hls:1.9.3")
+    implementation("androidx.media3:media3-exoplayer-dash:1.9.3")
+    implementation("androidx.media3:media3-ui:1.9.3")
+    implementation("org.jellyfin.media3:media3-ffmpeg-decoder:1.9.0+1")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 }
